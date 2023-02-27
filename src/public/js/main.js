fd03b4737a9e2b5cee32bdb514589b03150e6891
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-02-27 00:24:23 -0600

    v2

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,6 +4,29 @@ $(function() {
     const messageForm= $('#message-form');
     const messageBox= $('#message');
     const chat= $('#chat');
+    
+    const nickForm = $('#nickForm');
+    const nickError = $('#nickError');
+    const nickName = $('#nickName');
+
+    const users = $('#usernames');
+
+    nickForm.submit(e=>{
+        e.preventDefault();
+        socket.emit('new user',nickName.val(), data=>{
+            if(data){
+                $('#nickWrap').hide();
+                $('#contentWrap').removeClass('contentWrap');
+            }else{
+               nickError.html(`
+            <div class="font-bold text-red-700">
+                El nombre del usuario ya existe
+            </div>
+               `);
+            }
+            nickName.val('');
+        });
+    });
 
     messageForm.submit( e => {
         e.preventDefault();
@@ -11,8 +34,15 @@ $(function() {
         messageBox.val('');
     });
     socket.on('new message', function(data) {
-        chat.append(data + '<br/>');
+        chat.append('<b>' + data.nick + '</b>'+' : '+ data.msg + '<br/>');
+    });
 
+    socket.on('usernames', data=>{
+        let html= '';
+        for (let i=0; i<data.length; i++){
+            html += `<p><i class="fas fa-user"> </i>    ${data[i]}</p>`
+        }
+        users.html(html);
     });
 });
 
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,34 @@
 
 module.exports = function(io){
+    let nicknames = [];
     io.on('connection',socket =>{
         console.log('New user connected');
-        socket.on('message',function(data) {
-            io.sockets.emit('new message', data);
+        socket.on('message',data=> {
+            io.sockets.emit('new message', {
+                msg:data,
+                nick: socket.nickname
+            });
         });
+
+        socket.on('new user', (data,cb) => {
+            console.log(data);
+            if(nicknames.indexOf(data)!= -1) {
+            cb(false, data);
+            }else{
+                cb(true, data);
+                socket.nickname = data;
+                nicknames.push(socket.nickname);
+                updateNicknames();
+            }
+        });
+
+        socket.on('disconnect', data=>{
+            if(!socket.nickname)return;
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            updateNicknames();
+        });
+        function updateNicknames(){
+            io.sockets.emit('usernames', nicknames);
+        }
     });
 }
